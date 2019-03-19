ebe2bc2a3c649676d08b5a22b7b6088f66e86a48
devmisfit <dev@misfit.tech>
2019-03-19 17:48:58 +0600

    minor changes

--- app/src/main/java/id/co/skoline/view/activities/SignUpActivity.java
+++ app/src/main/java/id/co/skoline/view/activities/SignUpActivity.java
@@ -1,15 +1,18 @@
 package id.co.skoline.view.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 import id.co.skoline.R;
 
-public class SignUpActivity extends AppCompatActivity {
+public class SignUpActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+    }
+
+    @Override
+    protected void viewRelatedTask() {
+
     }
 }
