0f610aa1063d81390b0e9daae03e7a26e32be310
devmisfit <dev@misfit.tech>
2019-03-20 10:29:50 +0600

    signupbiniding variable added

--- app/src/main/java/id/co/skoline/view/activities/SignUpActivity.java
+++ app/src/main/java/id/co/skoline/view/activities/SignUpActivity.java
@@ -1,18 +1,23 @@
 package id.co.skoline.view.activities;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import id.co.skoline.R;
+import id.co.skoline.databinding.ActivitySignUpBinding;
 
 public class SignUpActivity extends BaseActivity {
+
+    ActivitySignUpBinding signUpBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        signUpBinding = DataBindingUtil.setContentView(this, R.layout.activity_sign_up);
+
     }
 
     @Override
     protected void viewRelatedTask() {
-
+        signUpBinding.sampleText.setText("Hello");
     }
 }
