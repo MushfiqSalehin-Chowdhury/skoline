e8fba48af43cff48304aa65242d7cef1d4594ab2
MushfiqSalehin-Chowdhury <mushfiq.chowdhury04@gmail.com>
2019-03-21 09:39:20 +0600

    modified

--- app/src/main/java/id/co/skoline/view/activities/MainActivity.java
+++ app/src/main/java/id/co/skoline/view/activities/MainActivity.java
@@ -1,7 +1,9 @@
 package id.co.skoline.view.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import id.co.skoline.R;
@@ -15,5 +17,9 @@ public class MainActivity extends AppCompatActivity {
 
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
+    }
+
+    public void signup(View view) {
+        startActivity(new Intent(this,SignUpActivity.class));
     }
 }
--- app/src/main/java/id/co/skoline/view/activities/SignUpActivity.java
+++ app/src/main/java/id/co/skoline/view/activities/SignUpActivity.java
@@ -1,13 +1,18 @@
 package id.co.skoline.view.activities;
 
 import android.databinding.DataBindingUtil;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
 import id.co.skoline.R;
 import id.co.skoline.databinding.ActivitySignUpBinding;
 
 public class SignUpActivity extends BaseActivity {
 
     ActivitySignUpBinding signUpBinding;
+    Spinner day,month,year;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,17 @@ public class SignUpActivity extends BaseActivity {
 
     @Override
     protected void viewRelatedTask() {
-        signUpBinding.sampleText.setText("Hello");
+        signUpBinding.sampleText.setText("helooooooooooooooooo"+"\n" +"oooooooooooooooooooo");
+        signUpBinding.sampleText.setTextColor(Color.WHITE);
+        signUpBinding.name.setHint("Enter Name");
+        signUpBinding.email.setHint("example@abc.com");
+        signUpBinding.lastname.setHint("Last Name");
+        signUpBinding.location.setHint("your location");
+        signUpBinding.sampleText1.setText("helooooooooooooooooo"+"\n" +"oooooooooooooooooooo");
+        signUpBinding.sampleText2.setText("Hellooooooooooooooooooooooooooooooooooooooooooooooooo");
+        signUpBinding.news.setText("helooooooooooooo ?");
+        signUpBinding.newss.setText("helooooooooooooooooo"+"\n" +"oooooooooooooooooooo");
+        signUpBinding.signup.setText("Dafter");
+
     }
 }
