263023e4d41362821dae59ac9d27f631123fd3c5
Mushfiq Salehin <chowdhurymushfiqsalehin@gmail.com>
2019-04-10 16:49:41 +0600

    country selector added,terms and condition link added

--- app/src/main/java/id/co/skoline/view/activities/SignUpActivity.java
+++ app/src/main/java/id/co/skoline/view/activities/SignUpActivity.java
@@ -7,6 +7,7 @@ import android.text.Html;
 import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import id.co.skoline.R;
 import id.co.skoline.databinding.ActivitySignUpBinding;
@@ -15,6 +16,7 @@ public class SignUpActivity extends BaseActivity {
 
     ActivitySignUpBinding signUpBinding;
     Spinner day,month,year;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +29,14 @@ public class SignUpActivity extends BaseActivity {
     protected void viewRelatedTask() {
         signUpBinding.terms.setText(Html.fromHtml(getString(R.string.agree_terms_privacy)));
         signUpBinding.terms.setMovementMethod(LinkMovementMethod.getInstance());
+
+
     }
 
     public void verify(View view) {
+
+        Toast.makeText(this, signUpBinding.ccp.getSelectedCountryCode(), Toast.LENGTH_SHORT).show();
+
         startActivity(new Intent(this,OtpActivity.class));
     }
 }
